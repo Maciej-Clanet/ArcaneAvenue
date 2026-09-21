8de00b8c5e076cddd98f26ebcef2237635c7e8ab
Maciej-Clanet <maciejclanet@gmail.com>
2026-09-21 10:22:10 +0100

    safasf

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main className="main-content test">
+        temp main
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default App
